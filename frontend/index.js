e03679fa083f477d778c34131a04a24d838210c1
Eric Martins <ericmartis123@gmail.com>
2025-07-07 10:09:35 -0300

    feat: initialize frontend structure with main App component and routing setup

---
+++ frontend/index.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [msg, setMsg] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/')
+      .then(res => setMsg(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>{msg}</h1>
+    </div>
+  );
+}
+
+export default App;
