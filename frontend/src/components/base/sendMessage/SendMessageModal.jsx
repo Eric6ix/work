8c1a463ad805e2175f3b8448d7e58feb403fdd31
Eric Martins <ericmartis123@gmail.com>
2025-07-12 11:39:35 -0300

    feat: enhance SendMessageModal; add form handling and file upload support

--- frontend/src/components/base/sendMessage/SendMessageModal.jsx
+++ frontend/src/components/base/sendMessage/SendMessageModal.jsx
@@ -3,13 +3,24 @@ import Modal from '../Modal';
 import './SendMessageModalStyle.css';
 
 const SendMessageModal = ({ onClose }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aqui você pode montar um FormData com os dados do formulário e os arquivos
+  };
+
   return (
     <Modal isOpen={true} onClose={onClose} title="Enviar Mensagem">
-      <form>
-        <label>Título:</label>
-        <input type="text" />
-        <label>Conteúdo:</label>
-        <textarea />
+      <form onSubmit={handleSubmit} encType="multipart/form-data">
+
+        <input placeholder='Qual o título da sua mensagem?' type="text" name="title" required />
+
+        <textarea placeholder='Digite o conteúdo da mensage aqui.' name="content" required />
+
+        <div className="file-box-content">
+          <label>Anexar arquivos (.pdf, .xml):</label>
+        <input type="file" name="attachments" multiple accept=".pdf,.xml"/>
+        </div>
+ 
         <button type="submit">Enviar</button>
       </form>
     </Modal>
